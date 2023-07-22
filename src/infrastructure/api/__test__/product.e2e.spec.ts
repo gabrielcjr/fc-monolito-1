13af7ef50beb50372b39784b40940071d85ab5f2
Paulo Henrique Oliveira dos Santos <paulohsantosol@gmail.com>
2023-07-22 15:49:39 -0300

    test: product e2e

---
+++ src/infrastructure/api/__test__/product.e2e.spec.ts
@@ -0,0 +1,30 @@
+import { app, sequelize } from '../express';
+import request from 'supertest';
+
+describe('E2E test for product', () => {
+	beforeEach(async () => {
+		await sequelize.sync({ force: true });
+	});
+
+	afterAll(async () => {
+		await sequelize.close();
+	});
+
+	it('should create a product', async () => {
+		const response = await request(app)
+			.post('/product')
+			.send({
+				name: 'Product A',
+				description: 'Product A Description',
+				purchasePrice: 10,
+				stock: 100,
+			});
+
+		expect(response.status).toBe(200);
+		expect(response.body.id).toBeDefined();
+		expect(response.body.name).toBe('Product A');
+		expect(response.body.description).toBe('Product A Description');
+		expect(response.body.purchasePrice).toBe(10);
+		expect(response.body.stock).toBe(100);
+	});
+});
