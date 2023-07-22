5bdec444984ef5ccadece84a5d72056749f531f1
Paulo Henrique Oliveira dos Santos <paulohsantosol@gmail.com>
2023-07-22 15:57:40 -0300

    feat: map client route

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -2,10 +2,13 @@ import express, { Express } from 'express';
 import { Sequelize } from 'sequelize-typescript';
 import { ProductModel } from '../../modules/product-adm/repository/product.model';
 import { productRoute } from './routes/product.route';
+import { clientRoute } from './routes/clients.route';
+import { ClientModel } from '../../modules/client-adm/repository/client.model';
 
 export const app: Express = express();
 app.use(express.json());
 app.use("/product", productRoute)
+app.use("/client", clientRoute)
 
 export let sequelize: Sequelize;
 
@@ -15,7 +18,7 @@ async function setupDb() {
     storage: ':memory:',
     logging: false,
   });
-  await sequelize.addModels([ProductModel]);
+  await sequelize.addModels([ProductModel, ClientModel]);
   await sequelize.sync();
 }
 setupDb();
