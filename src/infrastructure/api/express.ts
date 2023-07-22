8582ab4587cc7804044674e8d7725ea01b77df4c
Paulo Henrique Oliveira dos Santos <paulohsantosol@gmail.com>
2023-07-22 16:51:48 -0300

    feat: map invoice route

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -1,14 +1,18 @@
 import express, { Express } from 'express';
 import { Sequelize } from 'sequelize-typescript';
 import { ProductModel } from '../../modules/product-adm/repository/product.model';
+import { InvoiceProductModel } from '../../modules/invoice/repository/product.model';
+import { InvoiceModel } from '../../modules/invoice/repository/invoice.model';
 import { productRoute } from './routes/product.route';
 import { clientRoute } from './routes/clients.route';
 import { ClientModel } from '../../modules/client-adm/repository/client.model';
+import { invoiceRoute } from './routes/invoice.route';
 
 export const app: Express = express();
 app.use(express.json());
 app.use("/product", productRoute)
 app.use("/client", clientRoute)
+app.use("/invoice", invoiceRoute)
 
 export let sequelize: Sequelize;
 
@@ -18,7 +22,7 @@ async function setupDb() {
     storage: ':memory:',
     logging: false,
   });
-  await sequelize.addModels([ProductModel, ClientModel]);
+  await sequelize.addModels([ProductModel, InvoiceModel, InvoiceProductModel, ClientModel]);
   await sequelize.sync();
 }
 setupDb();
