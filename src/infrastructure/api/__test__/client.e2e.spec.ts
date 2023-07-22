875233af71bac7feed51a01e730b94f6e50e234b
Paulo Henrique Oliveira dos Santos <paulohsantosol@gmail.com>
2023-07-22 15:57:48 -0300

    test: client e2e

---
+++ src/infrastructure/api/__test__/client.e2e.spec.ts
@@ -0,0 +1,24 @@
+import { app, sequelize } from '../express';
+import request from 'supertest';
+
+describe('E2E test for client', () => {
+	beforeEach(async () => {
+		await sequelize.sync({ force: true });
+	});
+
+	afterAll(async () => {
+		await sequelize.close();
+	});
+
+	it('should create a client', async () => {
+		const response = await request(app)
+			.post('/client')
+			.send({
+				name: 'Client 1',
+				email: 'client@email.com',
+				address: 'Client address',
+			});
+
+		expect(response.status).toBe(200);
+	});
+});
