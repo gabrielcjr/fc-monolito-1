323182f9fae52b0d7920e6679dabdaba909633f6
Paulo Henrique Oliveira dos Santos <paulo.hosantos@einstein.br>
2023-07-25 22:50:25 -0300

    fix: client e2e test

--- src/infrastructure/api/__test__/client.e2e.spec.ts
+++ src/infrastructure/api/__test__/client.e2e.spec.ts
@@ -1,5 +1,5 @@
-import { app, sequelize } from '../express';
 import request from 'supertest';
+import { app, sequelize } from '../express';
 
 describe('E2E test for client', () => {
 	beforeEach(async () => {
@@ -16,7 +16,13 @@ describe('E2E test for client', () => {
 			.send({
 				name: 'Client 1',
 				email: 'client@email.com',
-				address: 'Client address',
+				document: "123",
+				street: "street 1",
+				number: "1",
+				complement: "",
+				city: "São Paulo",
+				state: "SP",
+				zipCode: "1234567890",
 			});
 
 		expect(response.status).toBe(200);
